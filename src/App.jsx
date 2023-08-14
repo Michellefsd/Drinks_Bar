a5803bab4ab009375c079b10228c43a1d14dab14
Michellefsd <promichfsd@gmail.com>
2023-08-14 00:16:51 -0300

    ajusto el header y colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import img from '../public/img/cottonbro.jpg'
 
 function App() {
 
@@ -12,13 +11,11 @@ function App() {
         <a href=""></a>
         <a href="">Contato</a>
       </nav>
-      <figure className='hero'>
-        <div className='back-img-hero'></div>
-        <img className='img-hero' src={img} alt="Gente brindando" width="" height="" title='O brindis que toda festa devería ter'/>
-        <figcaption className='figcaption'>Cottonbro</figcaption>
-      </figure>
-      <h1>Tenha a sua experiencia perfeita com nós.</h1>
-      <p>Nao acredita que nessas festas vamos deixar voce mais feliz que nunca? Com o nosso prestigioso servico de mais de 12 anos na ilha</p>
+      <header className='hero'>
+        <h1>Tenha a sua experiencia perfeita com nós.</h1>
+      </header>
+        <p>Nao acredita que nessas festas vamos deixar voce mais feliz que nunca? Com o nosso prestigioso servico de mais de 12 anos na ilha</p>
+      
     </>
   )
 }
